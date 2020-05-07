5c24d0190026320f065ad412267ae57907575507
Erik Ford <ekford18@gmail.com>
2020-05-07 14:25:26 -0700

    basic files completed

---
+++ data/characters.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+    name: 'Absolam the Genius',
+    level: 7,
+    alignment: 'Good',
+    is_alive: true,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Shiska of many Styles',
+    level: 8,
+    alignment: 'Evil',
+    is_alive: true,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Morriden the Changed',
+    level: 9,
+    alignment: 'Good',
+    is_alive: false,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Bast the Undefeated',
+    level: 5,
+    alignment: 'Evil',
+    is_alive: false,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Andar StormRider',
+    level: 7,
+    alignment: 'Good',
+    is_alive: true,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Kartanis of Bahamut',
+    level: 9,
+    alignment: 'Good',
+    is_alive: false,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Umbra the Cursed Blade',
+    level: 8,
+    alignment: 'Evil',
+    is_alive: true,
+    description: 'come back with cool descriptions'
+  },
+  {
+    name: 'Vissalis the Lizard',
+    level: 8,
+    alignment: 'Evil',
+    is_alive: true,
+    description: 'come back with cool descriptions'
+  },
+];
---
+++ data/load-seed-data.js
@@ -0,0 +1,46 @@
+const client = require('../lib/client');
+// import our seed data:
+const characters = require('./characters.js');
+const usersData = require('./users.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      characters.map(character => {
+        return client.query(`
+                    INSERT INTO characters (name, level, alignment, is_alive, description)
+                    VALUES ($1, $2, $3, $4, $5);
+                `,
+        [character.name, character.level, character.alignment, character.is_alive, character.description]);
+      })
+    );
+    
+
+    console.log('seed data load complete');
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
