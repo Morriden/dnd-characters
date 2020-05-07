4198eb49dd79117c0ddf6c22b2fa3154b828b59a
Erik Ford <ekford18@gmail.com>
2020-05-07 14:39:45 -0700

    correction

--- data/create-tables.js
+++ data/create-tables.js
@@ -16,7 +16,7 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE characters (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
                     level INTEGER NOT NULL,
