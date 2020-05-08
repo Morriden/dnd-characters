7720853c1307b13e7d9b5337043645a24f652bb9
Erik Ford <ekford18@gmail.com>
2020-05-08 14:27:06 -0700

    update heroku

--- server.js
+++ server.js
@@ -15,11 +15,23 @@ app.get('/characters', async(req, res) => {
 });
 
 app.get('/characters/:id', async(req, res) => {
+  const id = req.params.id;
   const data = await client.query(`
-  SELECT * from characters
-  WHERE id=${req.params.id};
-  `);
+  SELECT * from characters WHERE id=$1`,
+  [id]
+  );
 
+  res.json(data.rows);
+});
+
+app.post('/characters/', async(req, res) => {
+  console.log(req.body);
+  const data = await client.query(
+    `insert into characters (name, level, alignment, is_alive, description, owner_id)
+    values ($1, $2, $3, $4, $5, $6)
+    returning *`,
+    [req.body.name, req.body.level, req.body.alignment, req.body.is_alive, req.body.description, 1]
+  );
   res.json(data.rows);
 });
 
